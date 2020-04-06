8a3b5bccbcca4402647b3572d612ffa343965348
Pamela Sage <pamelasage3@gmail.com>
2020-04-06 05:28:56 -0700

    update projects

--- my-resume/src/Components/footer.js
+++ my-resume/src/Components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
         <div class="contact">
             <h2>Contact Information</h2>
             <address>
-                2740 W Country Gables Dr #215
+                2740 W Country Gables Dr #215 <br/>
                 Phoenix, AZ 85053
             </address>
             <span>(602) 580-2951</span>
