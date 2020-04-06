3ed76a8a4f63eeb3e73487b0e89a908b9137a824
Pamela Sage <pamelasage3@gmail.com>
2020-04-05 20:49:34 -0700

    moved components to src folder

--- my-resume/src/App.js
+++ my-resume/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Intro from '../Components/Intro';
-import Projects from '../Components/Projects';
+import Intro from './Components/Intro';
+import Projects from './Components/Projects';
 import './App.css';
 
 function App() {
